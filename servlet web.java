c2cc11c7c7b5f6128418789479e8da37dbc9082d
Aryandwivedi123 <80156852+Aryandwivedi123@users.noreply.github.com>
2021-06-01 21:58:57 +0530

    Add files via upload

---
+++ servlet web.java
@@ -0,0 +1,14 @@
+
+
+<servlet>  
+    <description></description>  
+    <display-name>LoginServlet</display-name>  
+    <servlet-name>LoginServlet</servlet-name>  
+    <servlet-class>LoginServlet</servlet-class>  
+  </servlet>  
+  <servlet-mapping>  
+    <servlet-name>LoginServlet</servlet-name>  
+    <url-pattern>/LoginServlet</url-pattern>  
+  </servlet-mapping>  
+   
+</web-app>  
